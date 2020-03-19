aca2a1783402e1f2c0b2e2f341da4f925bdc5f6a
FXXDEV <fxxdev@outlook.com>
2020-03-18 21:16:41 -0300

    Removed readFile and fixNullPointers

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -11,15 +11,12 @@ import java.util.Map;
 import java.util.PrimitiveIterator;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-
 import javax.swing.JOptionPane;
-
 
 
 public class Hashtable {
  
     Random rand = new Random();
-    private static ReadFile rf = new ReadFile();
     private static ArrayList<Node> activeNodes = new ArrayList<>();
     private static StringBuilder actives = new StringBuilder();
     private static StringBuilder inactives = new StringBuilder();
@@ -27,14 +24,24 @@ public class Hashtable {
     private static ArrayList arrayList;
 
 
-    private Map<Integer, Node> musics;
-
     public Hashtable() throws IOException{
         ring = new HashMap<Integer,Node>();
-      
+        this.mountRing();
+    }    
+    
+    public ArrayList mountList() throws IOException {
+	    Path file = Paths.get("..\\Chord\\src\\files\\musics");
+        ArrayList arrayList = new ArrayList();
+        List<String> musicsFile = Files.readAllLines(file);
+
+        int i = 0;
+        for (String m : musicsFile) {
+            i++;
+            arrayList.add(m);
+        }
+        return arrayList;
     }
-
-
+    
     static void sweepHash(){
     	activeNodes.clear();
         ArrayList<Node> previousNodes = new ArrayList<>();
@@ -74,7 +81,7 @@ public class Hashtable {
     
     public void mountRing() throws IOException {
         Node node = new Node();
-        arrayList = rf.mountList();
+        arrayList = this.mountList();
         
         int i = 0;
         for (Object m : arrayList) {
@@ -84,23 +91,19 @@ public class Hashtable {
             node.setActive(false);
             i++;
             ring.put(node.getKey(), node);
-            
-            
         }
 
     }
     
-    
-
     public static void nodeStatus(){
     	inactives.setLength(0);
     	actives.setLength(0);
 
         for (int i=0; i<ring.size(); i++){
             if(!(ring.get(i).isActive())){
-            	inactives.append("Inactive: "+ring.get(i).getKey()+"\n");
+            	inactives.append("Inativo: "+ring.get(i).getKey()+"\n");
             }else{
-            	actives.append("Active: "+ring.get(i).getKey()+"\n");
+            	actives.append("Ativo: "+ring.get(i).getKey()+"\n");
             }
         }
 
@@ -111,13 +114,12 @@ public class Hashtable {
 
         for (int i=0; i<activeNodes.size();i++){
             for (int j=0; j< activeNodes.get(i).getMusics().size(); j++){
-                System.out.println("sad");
                 if(activeNodes.get(i).getMusics().get(j).getMusic().equalsIgnoreCase(music)){
-                    return "Music " + music + "are in node: "+activeNodes.get(i).getMusics().get(j).getKey()+ "found in active node:"+activeNodes.get(i).getKey();
+                    return "A musica " + music + " esta no no " + activeNodes.get(i).getMusics().get(j).getKey()+ " que esta vinculado ao no: " + activeNodes.get(i).getKey();
                 }
             }
         }
-        return "Not found";
+        return "Musica nao encontrada!";
     }
 
     public static void main(String[] args) throws IOException {
@@ -127,62 +129,66 @@ public class Hashtable {
 
       PrimitiveIterator.OfInt it = ThreadLocalRandom.current().ints(0, arrayList.size()-1).distinct().iterator();
       
-      
-      
-      for (int i=0; i < 8; i++){
+      for (int i=0; i<8; i++){
           int number = it.next();
           ring.get(number).setActive(true);
-          System.out.println(("Active node: " + number));
+          System.out.println(("No ativo: " + number));
       }
       
       nodeStatus();
       sweepHash();
 
-      
       for(;;) {
     	  if (key.equals("1")) {
-              String aux = JOptionPane.showInputDialog(null, "Type a node's key to activateor type 'x' to return to main menu\n" + inactives, "Insertion", JOptionPane.QUESTION_MESSAGE);
-              if(aux == null || aux.isEmpty()){
-                  JOptionPane.showMessageDialog(null,"Type a value before continue!");
+              String aux = JOptionPane.showInputDialog(null, "Digite a chave do no para ativar ou digite 'X' para retornar ao menu principal \n" + inactives, "Insercao", JOptionPane.QUESTION_MESSAGE);
+              if(aux.isBlank() || aux == null || aux.isEmpty()){
+                  JOptionPane.showMessageDialog(null,"Digte um valor para continuar..");
                   key = "0";
               }else if(aux.equalsIgnoreCase("x")) {
                   key = "0";
-              }else {
+              }else{
                   int value = Integer.parseInt(aux);
                   System.out.println(value);
                   if (value < ring.size() && !(ring.get(value).getActive())) {
                       ring.get(value).setActive(true);
                       sweepHash();
                       nodeStatus();
+                      JOptionPane.showMessageDialog(null,aux + " agora esta ativo!");
+                  } else {
+                	  JOptionPane.showMessageDialog(null, aux + " ja esta ativo!");
                   }
               }
           } else if (key.equals("2")) {
-              String aux = JOptionPane.showInputDialog(null, "Type a node key to turn off or type 'x'  to return to main menu\n" + actives, "Remove", JOptionPane.QUESTION_MESSAGE);
-              if(aux == null || aux.isEmpty()){
-                  JOptionPane.showMessageDialog(null,"Type a value before continue!");
+              String aux = JOptionPane.showInputDialog(null, "Digite a chave de um no para desativar ou digite 'X' para retornar ao menu principal \n" + actives, "Remover", JOptionPane.QUESTION_MESSAGE);
+              if(aux.isBlank() || aux == null || aux.isEmpty()){
+                  JOptionPane.showMessageDialog(null,"Digite um valor antes de continuar!");
                   key = "0";
               }else if(aux.equalsIgnoreCase("x")) {
                   key = "0";                 
               }else{
                   int value = Integer.parseInt(aux);
-                  System.out.println(value);
                   if (value < ring.size() && ring.get(value).getActive()) {
                       ring.get(value).setActive(false);
                       sweepHash();
                       nodeStatus();
+                      JOptionPane.showMessageDialog(null,aux + " agora esta desativado!");
+                  } else {
+                	  JOptionPane.showMessageDialog(null, aux + " ja esta desativado !");
                   }
               }
           } else if (key.equals("3")) {
-              String musica = JOptionPane.showInputDialog(null, "Choose a music: ");
-              if(musica.isEmpty() || musica == null){
-                  JOptionPane.showMessageDialog(null,"Type a value before continue!");
+              String music = JOptionPane.showInputDialog(null, "Escolha uma musica: ");
+              if(music.isEmpty() || music.isBlank() || music == null){
+                  JOptionPane.showMessageDialog(null,"Digite um valor para continuar!");
                   key = "0";
               }else {
-                  JOptionPane.showMessageDialog(null, search(musica));
+                  JOptionPane.showMessageDialog(null, search(music));
                   key = "0";
               }
           }else{
-        	  key = JOptionPane.showInputDialog("1 - Activate node \n" + "2 - Desactivate node\n" + "3 - Search music");
+        	  key = JOptionPane.showInputDialog("1 - Ativar no"
+        	  		+ "\n2 - Desativar no\n"
+        	  		+ "3 - Pesquisar musica");
               if(key == null){
                   break;
               }
@@ -192,4 +198,6 @@ public class Hashtable {
 
     }
 }
+
+
 
